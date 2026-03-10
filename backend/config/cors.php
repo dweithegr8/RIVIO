a6bfd5744bbe7e8cbd852f7a8a8e41b58f105ff6
michaeljosherr <mbargabino.k12042471@umak.edu.ph>
2026-03-10 13:36:20 +0800

    update yml file

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -22,6 +22,7 @@ return [
         'http://localhost:3000',
         'http://127.0.0.1:5173',
         'http://127.0.0.1:3000',
+        env('APP_URL'),
     ],
 
     'allowed_origins_patterns' => [],
